db82e7824e1480034b8a9f7bc03de95c89e97e78
Ashton Medina <ashtonmedina22@gmail.com>
2025-12-02 15:40:29 -0600

    Create python_if_else_assignment.py

---
+++ python_if_else_assignment.py
@@ -0,0 +1,6 @@
+current_temperature = int(input("What is the current temperature? "))
+
+if current_temperature >= 20:
+    print("Have a nice sunny day")
+else:
+    print("Don't forget your jacket")
