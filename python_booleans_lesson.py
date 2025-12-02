a97b0d2aca2d548a9b9de761d58945f347a80353
Ashton Medina <ashtonmedina22@gmail.com>
2025-12-02 15:32:13 -0600

    Create python_booleans_lesson.py

---
+++ python_booleans_lesson.py
@@ -0,0 +1,8 @@
+height = 170
+age = 31
+
+height != 160  # True
+height > 170 and age > 12  # False
+height < 170 or age >= 12  # True
+height >= 170 and age >= 31  # True
+not(height >= 170 and age >= 31)  # False
