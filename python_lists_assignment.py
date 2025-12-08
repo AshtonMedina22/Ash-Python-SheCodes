0bfc89fcf714228bbb2758d857e65b482c1f8c8e
Ashton Medina <ashtonmedina22@gmail.com>
2025-12-08 15:53:00 -0600

    Create python_lists_assignment.py

---
+++ python_lists_assignment.py
@@ -0,0 +1,26 @@
+# Create a list of 3 countries you'd like to visit
+countries = ["Japan", "Sri Lanka", "Kenya"]
+
+# Print out the list
+print(countries)
+
+# Remove the last country
+countries.pop()
+
+# Print out the list
+print(countries)
+
+# Add another element at the beginning of the list
+countries.insert(0, "Georgia")
+
+# Print out the list
+print(countries)
+
+# Print out the length of the list
+print(len(countries))
+
+# Sort the list
+countries.sort()
+
+# Print out the list
+print(countries)
